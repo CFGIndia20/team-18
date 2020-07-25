15fd04a31e88b6e9650193570407e02119b5fdd1
Rohit <rohitrocks2801@gmail.com>
2020-07-25 20:17:17 +0530

    Completed Login, SignUp Functionality with Sessions

--- templates/authentication/signup.php
+++ templates/authentication/signup.php
@@ -17,31 +17,58 @@
         ?>
         <div class="container dim mt-5 p-3 shadow-lg bg-white rounded">
             <h3 class="text-center mb-4">SignUp</h3>
-            <form action="" method="POST">
+
+            <?php
+                include('../includes/db.php');
+                if($connection){
+                    // for sign-in
+                    if(isset($_POST['sign'])){
+                        include('../includes/hash.php');
+
+                        $name = $_POST['name'];
+                        $role = $_POST['role'];
+                        $password = secret($_POST['password']);
+                        $mobile = $_POST['mobile'];
+
+                        $query = "SELECT * FROM users WHERE mobile = {$mobile}";
+                        $feth_query =  mysqli_fetch_array(mysqli_query($connection, $query));
+                        
+                        if($feth_query!=null){
+                            echo "<h6 class='text-center mb-3' style='color: red;'>Mobile Already Registered!</h6>";
+                        } else {
+                            $query = "INSERT INTO users(username, passwrd, position, mobile) VALUES('{$name}', '{$password}', '{$role}', {$mobile})";
+                            $create_user_query = mysqli_query($connection, $query);
+                            echo "<h6 class='text-center mb-3' style='color: green;'>Registeration Successfull!</h6>";
+                        }
+                    }
+                }
+            ?>
+
+            <form action="#" method="POST">
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" name="name">
+                    <input type="text" class="form-control" id="name" name="name" required>
                 </div>
                 <div class="form-group">
                     <label for="mobile">Mobile Number</label>
-                    <input type="number" min="1000000000" max="9999999999" oninvalid="this.setCustomValidity('Invalid Mobile No.!')" oninput="this.setCustomValidity('')" class="form-control" id="mobile" name="mobile">
+                    <input type="number" min="1000000000" max="9999999999" oninvalid="this.setCustomValidity('Invalid Mobile No.!')" oninput="this.setCustomValidity('')" class="form-control" id="mobile" name="mobile" required>
                 </div>
                 <label>Role</label>
-                <select class="form-control mb-3">
-                    <option selected disabled>Select Role</option>
+                <select class="form-control mb-3" name="role" required>
+                    <!-- <option selected disabled>Select Role</option> -->
                     <option value=0>Women Employee</option>
                     <option value=1>Task Manager</option>
                 </select>
-                <div class="form-group">
+                <!-- <div class="form-group">
                     <label for="email">Email-ID</label>
                     <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
+                </div> -->
                 <div class="form-group">
                     <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" name="password">
+                    <input type="password" class="form-control" id="password" name="password" required>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary" name="sign">Submit</button>
             </form>
             <label>Have an account? <a href="./login.php">Login</a></label>
         </div>
---
+++ templates/includes/session.php
@@ -0,0 +1,26 @@
+<?php
+    function des_sess(){
+        // Start the session
+        if(!isset($_SESSION)){
+            session_start();
+        }
+
+        // remove all session variables
+        session_unset();
+
+        // destroy the session
+        session_destroy();
+
+        return 1;
+    }
+    
+
+    function set_sess($id){
+        // Start the session
+        if(!isset($_SESSION)){
+            session_start();
+        }
+
+        $_SESSION['id'] = $id;
+    }
+?>
