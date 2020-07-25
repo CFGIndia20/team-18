4d35d00baec98a85ea395fac5182aa4387403ffc
Rohit <rohitrocks2801@gmail.com>
2020-07-26 01:25:05 +0530

    html to php needs and sql structure

---
+++ templates/task/task.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+      include('../includes/header.php');
+    ?>
+    <title>Tasks</title>
+
+    <!-- Mobile Specific Meta
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.png" />
+
+    <!-- CSS
+    ================================================== -->
+    <!-- Themefisher Icon font -->
+    <link rel="stylesheet" href="../plugins/themefisher-font/style.css">
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Lightbox.min css -->
+    <link rel="stylesheet" href="../plugins/lightbox2/dist/css/lightbox.min.css">
+    <!-- animation css -->
+    <link rel="stylesheet" href="../plugins/animate/animate.css">
+    <!-- Slick Carousel -->
+    <link rel="stylesheet" href="../plugins/slick/slick.css">
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="../css/style.css">  
+  </head>
+
+  <body id="body">
+    <?php
+      include('../includes/navbar.php');
+    ?>
+    <section class="blog" id="blog">
+      <div class="container">
+        <div class="row">
+
+          <!-- section title -->
+          <div class="col-12">
+            <div class="title text-center ">
+              <h2> Pending <span class="color">Tasks</span></h2>
+              <div class="border"></div>
+            </div>
+          </div>
+          <!-- /section title -->
+          
+          <!-- single blog post -->
+          <article class="col-md-4 col-sm-6 col-xs-12 clearfix ">
+            <div class="post-item">
+              <div class="media-wrapper">
+                <img src="../images/task3.jpg" alt="amazing caves coverimage" class="img-fluid">
+              </div>
+
+              <div class="content">
+                <h3><a href="trackProgress.html">Task1</a></h3>
+                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+                  moon officia aute, non skateboard dolor brunch.</p>
+                <a class="btn btn-main " href="trackProgress.php">Update Progress</a>
+              </div>
+            </div>
+          </article>
+          <!-- /single blog post -->
+
+          <!-- single blog post -->
+          <article class="col-md-4 col-sm-6 col-xs-12 ">
+            <div class="post-item">
+              <div class="media-wrapper">
+                <img src="../images/task2.jpg" alt="amazing caves coverimage" class="img-fluid">
+              </div>
+
+              <div class="content">
+                <h3><a href="trackProgress.html">Task2</a></h3>
+                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+                  moon officia aute, non skateboard dolor brunch.</p>
+                <a class="btn btn-main" href="trackProgress.php">Update Progress</a>
+              </div>
+            </div>
+          </article>
+          <!-- end single blog post -->
+
+          <!-- single blog post -->
+          <article class="col-md-4 col-sm-6 col-xs-12 ">
+            <div class="post-item">
+              <div class="media-wrapper">
+                <img src="../images/task3.jpg" alt="amazing caves coverimage" class="img-fluid">
+              </div>
+
+              <div class="content">
+                <h3><a href="trackProgress.html">Task3</a></h3>
+                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+                  moon officia aute, non skateboard dolor brunch.</p>
+                <a class="btn btn-main" href="trackProgress.php">Update Progress</a>
+              </div>
+            </div>
+          </article>
+          <!-- end single blog post -->
+        </div> <!-- end row -->
+      </div> <!-- end container -->
+    </section> <!-- end section -->
+
+
+    <!-- 
+    Essential Scripts
+    =====================================-->
+    <!-- Main jQuery -->
+    <script src="../plugins/jquery/jquery.min.js"></script>
+    <!-- Google Map -->
+    <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script> -->
+    <!-- <script  src="../plugins/google-map/gmap.js"></script> -->
+
+    <!-- Form Validation -->
+    <script src="../plugins/form-validation/jquery.form.js"></script> 
+    <script src="../plugins/form-validation/jquery.validate.min.js"></script>
+    
+    <!-- Bootstrap4 -->
+    <script src="../plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Parallax -->
+    <script src="../plugins/parallax/jquery.parallax-1.1.3.js"></script>
+    <!-- lightbox -->
+    <script src="../plugins/lightbox2/dist/js/lightbox.min.js"></script>
+    <!-- Owl Carousel -->
+    <script src="../plugins/slick/slick.min.js"></script>
+    <!-- filter -->
+    <script src="../plugins/filterizr/jquery.filterizr.min.js"></script>
+    <!-- Smooth Scroll js -->
+    <script src="../plugins/smooth-scroll/smooth-scroll.min.js"></script>
+    
+    <!-- Custom js -->
+    <script src="../js/script.js"></script>
+
+  </body>
+</html>
