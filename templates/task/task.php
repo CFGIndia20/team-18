fdedc64eacd23f47b52c278a651515cf23d0a5a9
Diksha <dikshasinghania1218@gmail.com>
2020-07-26 05:08:20 +0530

    connecting task.php to database

--- templates/task/task.php
+++ templates/task/task.php
@@ -1,114 +1,198 @@
 <!DOCTYPE html>
-<html>
-  <head>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<?php
+session_start();
+
+include('../includes/db.php');
+if(isset($_SESSION['id'])&& $connection){
+    
+    //echo "hey";
+    $mobile = $_SESSION['id'];
+    $currdate = date("Y-m-d");
+    $query = "select productName,productsMade from trackproduct where dates>$currdate and mobile = $mobile";
+    //$query1 = "select productsMade from trackproduct where dates>$currdate and mobile = $mobile";
+    $result = mysqli_query($connection, $query);
+    //$result1 = mysqli_query($connection, $query1);
+    $i= mysqli_num_rows($result);
+    //echo $i;
+    
+    
+}
+
+?>
+
+<html> <!--<![endif]-->
+<head>
     <?php
-      include('../includes/header.php');
+        
+        include('../includes/header.php');
     ?>
-    <title>Tasks</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="Bingo One page parallax responsive HTML Template ">
+  
+  <meta name="author" content="Themefisher.com">
 
-    <!-- Mobile Specific Meta
-    ================================================== -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bingo | Responsive Multipurpose Parallax HTML5 Template</title>
 
-    <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.png" />
+<!-- Mobile Specific Meta
+  ================================================== -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.png" />
+  
+  <!-- CSS
+  ================================================== -->
+  <!-- Themefisher Icon font -->
+  <link rel="stylesheet" href="../plugins/themefisher-font/style.css">
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css">
+  <!-- Lightbox.min css -->
+  <link rel="stylesheet" href="../plugins/lightbox2/dist/css/lightbox.min.css">
+  <!-- animation css -->
+  <link rel="stylesheet" href="../plugins/animate/animate.css">
+  <!-- Slick Carousel -->
+  <link rel="stylesheet" href="../plugins/slick/slick.css">
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="../css/style.css">  
 
-    <!-- CSS
-    ================================================== -->
-    <!-- Themefisher Icon font -->
-    <link rel="stylesheet" href="../plugins/themefisher-font/style.css">
-    <!-- bootstrap.min css -->
-    <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css">
-    <!-- Lightbox.min css -->
-    <link rel="stylesheet" href="../plugins/lightbox2/dist/css/lightbox.min.css">
-    <!-- animation css -->
-    <link rel="stylesheet" href="../plugins/animate/animate.css">
-    <!-- Slick Carousel -->
-    <link rel="stylesheet" href="../plugins/slick/slick.css">
-    <!-- Main Stylesheet -->
-    <link rel="stylesheet" href="../css/style.css">  
-  </head>
+</head>
 
-  <body id="body">
+<body id="body">
     <?php
-      include('../includes/navbar.php');
+        include('../includes/navbar.php');
     ?>
-    <section class="blog" id="blog">
-      <div class="container">
-        <div class="row">
 
-          <!-- section title -->
-          <div class="col-12">
-            <div class="title text-center ">
-              <h2> Pending <span class="color">Tasks</span></h2>
-              <div class="border"></div>
+
+  
+
+<!--
+Fixed Navigation
+==================================== -->
+<header class="navigation fixed-top">
+ 
+</header>
+
+
+<section class="blog" id="blog">
+	<div class="container">
+		<div class="row">
+
+			<!-- section title -->
+			<div class="col-12">
+				<div class="title text-center ">
+					<h2> Pending <span class="color">Tasks</span></h2>
+					<div class="border"></div>
+					
+				</div>
+			</div>
+			<!-- /section title -->
+			<!-- single blog post -->
+                        <?php
+                            while($row=mysqli_fetch_assoc($result)){
+                                
+                            ?>
+			<article class="col-md-4 col-sm-6 col-xs-12 clearfix ">
+				<div class="post-item">
+					<div class="media-wrapper">
+            <div>
+              <?php
+//                $txt='ఇది ఒక
+//                పూర�?తి చేయాల�?సిన పని పెండింగ�?‌లో ఉంది. దయచేసి YouTube వీడియోన�? చూడండి మరియ�? వెబ�?‌సైట�?‌న�? ఎలా ఉపయోగించాలో తెల�?స�?కోండి. దయచేసి మీ హోంవర�?క�?‌న�? వెబ�?‌సైట�?‌లో అప�?‌లోడ�? చేయండి.';
+//                $txt=htmlspecialchars($txt);
+//                $txt=rawurlencode($txt);
+//                $html=file_get_contents('https://translate.google.com/translate_tts?ie=UTF-8&client=gtx&q='.$txt.'&tl=te-IN');
+//                $player="<audio controls='controls' autoplay><source src='data:audio/mpeg;base64,".base64_encode($html)."'></audio>";
+//                echo $player;
+//              ?>
             </div>
-          </div>
-          <!-- /section title -->
-          
-          <!-- single blog post -->
-          <article class="col-md-4 col-sm-6 col-xs-12 clearfix ">
-            <div class="post-item">
-              <div class="media-wrapper">
-                <img src="../images/task3.jpg" alt="amazing caves coverimage" class="img-fluid">
-              </div>
+						<img src="../images/task3.jpg" alt="amazing caves coverimage" class="img-fluid">
+					</div>
+                                    
 
-              <div class="content">
-                <h3><a href="trackProgress.html">Task1</a></h3>
-                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                  moon officia aute, non skateboard dolor brunch.</p>
-                <a class="btn btn-main " href="trackProgress.php">Update Progress</a>
-              </div>
-            </div>
-          </article>
-          <!-- /single blog post -->
+					<div class="content">
+                                            <h3><a href="trackProgress.php?pname=<?php echo $row['prodName']?>"></a> <?php echo $row['productName']?></h3>
+						<p>Task done: <?php echo $row['productsMade'];?></p>
+                                                <a class="btn btn-main" href="trackProgress.php?pname=<?php echo $row['productName']?>">Update Progress</a>
+					</div>
+				</div>
+			</article>
+                        <?php
+                            }
+                        ?>
+			
+		</div> <!-- end row -->
+	</div> <!-- end container -->
+</section> <!-- end section -->
 
-          <!-- single blog post -->
-          <article class="col-md-4 col-sm-6 col-xs-12 ">
-            <div class="post-item">
-              <div class="media-wrapper">
-                <img src="../images/task2.jpg" alt="amazing caves coverimage" class="img-fluid">
-              </div>
+<footer id="footer" class="bg-one">
+  <div class="top-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-3 col-lg-3">
+          <h3>about</h3>
+          <p>Integer posuere erat a ante venenati dapibus posuere velit aliquet. Fusce dapibus, tellus cursus commodo, tortor mauris sed posuere.</p>
+        </div>
+        <!-- End of .col-sm-3 -->
 
-              <div class="content">
-                <h3><a href="trackProgress.html">Task2</a></h3>
-                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                  moon officia aute, non skateboard dolor brunch.</p>
-                <a class="btn btn-main" href="trackProgress.php">Update Progress</a>
-              </div>
-            </div>
-          </article>
-          <!-- end single blog post -->
+        <div class="col-sm-3 col-md-3 col-lg-3">
+          <ul>
+            <li><h3>Our Services</h3></li>
+            <li><a href="#">Graphic Design</a></li>
+            <li><a href="#">Web Design</a></li>
+            <li><a href="#">Web Development</a></li>
+          </ul>
+        </div>
+        <!-- End of .col-sm-3 -->
 
-          <!-- single blog post -->
-          <article class="col-md-4 col-sm-6 col-xs-12 ">
-            <div class="post-item">
-              <div class="media-wrapper">
-                <img src="../images/task3.jpg" alt="amazing caves coverimage" class="img-fluid">
-              </div>
+        <div class="col-sm-3 col-md-3 col-lg-3">
+          <ul>
+            <li><h3>Quick Links</h3></li>
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">FAQ’s</a></li>
+            <li><a href="#">Badges</a></li>
+          </ul>
+        </div>
+        <!-- End of .col-sm-3 -->
 
-              <div class="content">
-                <h3><a href="trackProgress.html">Task3</a></h3>
-                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                  moon officia aute, non skateboard dolor brunch.</p>
-                <a class="btn btn-main" href="trackProgress.php">Update Progress</a>
-              </div>
-            </div>
-          </article>
-          <!-- end single blog post -->
-        </div> <!-- end row -->
-      </div> <!-- end container -->
-    </section> <!-- end section -->
+        <div class="col-sm-3 col-md-3 col-lg-3">
+          <ul>
+            <li><h3>Connect with us Socially</h3></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Youtube</a></li>
+            <li><a href="#">Pinterest</a></li>
+          </ul>
+        </div>
+        <!-- End of .col-sm-3 -->
+
+      </div>
+    </div> <!-- end container -->
+  </div>
+  <div class="footer-bottom">
+    <h5>Copyright 2020. All rights reserved.</h5>
+    <h6>Design and Developed by <a href="">UMEED</a></h6>
+  </div>
+</footer> <!-- end footer -->
 
 
+    <!-- end Footer Area
+    ========================================== -->
+
+
+    
     <!-- 
     Essential Scripts
     =====================================-->
     <!-- Main jQuery -->
     <script src="../plugins/jquery/jquery.min.js"></script>
     <!-- Google Map -->
-    <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script> -->
-    <!-- <script  src="../plugins/google-map/gmap.js"></script> -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script>
+    <script  src="../plugins/google-map/gmap.js"></script>
 
     <!-- Form Validation -->
     <script src="../plugins/form-validation/jquery.form.js"></script> 
@@ -131,4 +215,4 @@
     <script src="../js/script.js"></script>
 
   </body>
-</html>
+  </html>
